77c834a8d277e1bf18b3a6c5c4cf85a7919c779e
nexuslifeline <nexuslifeline@gmail.com>
2020-05-20 08:07:29 +0800

    updated readme. added student api

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Student;
+use Illuminate\Http\Request;
+use App\Http\Resources\StudentResource;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $perPage = $request->perPage ?? 20;
+        $students = !$request->has('paginate') || $request->paginate === 'true'
+            ? Student::paginate($perPage)
+            : Student::all();
+        return StudentResource::collection(
+            $students
+        );
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $student = Student::create($data);
+        return (new StudentResource($student))
+            ->response()
+            ->setStatusCode(201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Student $student)
+    {
+        return new StudentResource($student);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Student $student)
+    {
+        $data = $request->all();
+        $success = $student->update($data);
+        if ($success) {
+            return new StudentResource(
+                Student::find($studentId)
+            );
+        }
+        return response()->json([], 400); // Note! add error here
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Student $student)
+    {
+        $student->delete();
+        return response()->json([], 204);
+    }
+
+}
