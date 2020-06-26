1c4800a175f74eed6b947e02aca9de705a92ed17
edprnl <pecsonerick.ep@gmail.com>
2020-06-26 16:42:48 +0800

    migration updates
    migration updates

---
+++ database/migrations/2020_06_26_041311_add_approval_notes_on_student_fees_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddApprovalNotesOnStudentFeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('student_fees', function (Blueprint $table) {
+            $table->string('approval_notes', 1255)->after('student_fee_status_id')->default('')->nullable();
+            $table->integer('approved_by', 1255)->after('approval_notes')->nullable();
+            $table->dateTime('approved_date', 1255)->after('approved_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('student_fees', function (Blueprint $table) {
+            $table->dropColumn('approval_notes');
+            $table->dropColumn('approved_by');
+            $table->dropColumn('approved_date');
+        });
+    }
+}
