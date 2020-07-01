e226f4faa38f2a252f6796e17ff829535d40aa1b
Erick Pecson <pecsonerick.ep@gmail.com>
2020-07-01 22:37:32 +0800

    curriculum table migration
    curriculum table migration, subject prerequisite

---
+++ database/migrations/2020_07_01_132702_create_curriculums_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurriculumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curriculums', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->default('')->nullable();
+            $table->string('major')->default('')->nullable();
+            $table->unsignedBigInteger('effectiveYear')->nullable();
+            $table->foreign('school_category_id')->references('id')->on('school_categories');
+            $table->unsignedBigInteger('school_category_id')->nullable();
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->unsignedBigInteger('course_id')->nullable();
+            $table->text('notes')->nullable();
+            $table->integer('deleted_by')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->integer('created_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curriculums');
+    }
+}
