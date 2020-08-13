60450bff919183258e6fd8551a1f82f45a8f7aa0
Erick Pecson <pecsonerick.ep@gmail.com>
2020-08-13 22:39:40 +0800

    evaluation file refactor
    evaluation file refactor

--- app/Http/Controllers/EvaluationFileController.php
+++ app/Http/Controllers/EvaluationFileController.php
@@ -7,13 +7,16 @@ use App\EvaluationFile;
 use App\Http\Resources\EvaluationFileResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use App\Services\EvaluationFileService;
+use App\Http\Requests\EvaluationFileStoreRequest;
+use App\Http\Requests\EvaluationFileUpdateRequest;
 
 class EvaluationFileController extends Controller
 {
-    public function index(Request $request, $evaluttionId)
+    public function index(Request $request, $evaluationId)
     {
         $perPage = $request->per_page ?? 20;
-        $query = Evaluation::where('id', $evaluttionId)->first()->files();
+        $query = Evaluation::where('id', $evaluationId)->first()->files();
         $files = !$request->has('paginate') || $request->paginate === 'true'
             ? $query->paginate($perPage)
             : $query->get();
@@ -22,21 +25,37 @@ class EvaluationFileController extends Controller
         );
     }
 
-    public function store(Request $request, $evaluationId)
+    public function store(EvaluationFileStoreRequest $request, $evaluationId)
     {
+        // try {
+        //     $this->validate($request, [
+        //         'file' => 'required'
+        //     ]);
+
+        //     $path = $request->file('file')->store('files/evaluation');
+        //     $evaluationFile = EvaluationFile::create([
+        //         'evaluation_id' => $evaluationId,
+        //         'path' => $path,
+        //         'name' => $request->file('file')->getClientOriginalName()
+        //     ]);
+        //     return (new EvaluationFileResource($evaluationFile))
+        //     ->response()
+        //     ->setStatusCode(201);
+        // } catch (Throwable $e) {
+        //     Log::error('Message occured => ' . $e->getMessage());
+        //     return response()->json([], 400);
+        // }
+
+        
+        
+
         try {
-            $this->validate($request, [
-                'file' => 'required'
-            ]);
-            $path = $request->file('file')->store('files/evaluation');
-            $evaluationFile = EvaluationFile::create([
-                'evaluation_id' => $evaluationId,
-                'path' => $path,
-                'name' => $request->file('file')->getClientOriginalName()
-            ]);
+            $file = $request->file('file');
+            $evaluationFileService = new EvaluationFileService();
+            $evaluationFile = $evaluationFileService->store($evaluationId, $file);
             return (new EvaluationFileResource($evaluationFile))
-            ->response()
-            ->setStatusCode(201);
+                ->response()
+                ->setStatusCode(201);
         } catch (Throwable $e) {
             Log::error('Message occured => ' . $e->getMessage());
             return response()->json([], 400);
@@ -50,32 +69,52 @@ class EvaluationFileController extends Controller
     }
 
 
-    public function update(Request $request, $evaluationId,  $fileId)
+    public function update(EvaluationFileUpdateRequest $request, $evaluationId,  $fileId)
     {
-        $this->validate($request, [
-            'notes' => 'required',
-        ]);
+        // $this->validate($request, [
+        //     'notes' => 'required',
+        // ]);
 
-        $data = $request->all();
+        // $data = $request->all();
 
-        $evaluationFile = EvaluationFile::find($fileId);
+        // $evaluationFile = EvaluationFile::find($fileId);
         
-        $success = $evaluationFile->update($data);
+        // $success = $evaluationFile->update($data);
     
-        if ($success) {
+        // if ($success) {
+        //     return (new EvaluationFileResource($evaluationFile))
+        //         ->response()
+        //         ->setStatusCode(200);
+        // }
+
+        try {
+            $data = $request->all();
+            $evaluationFileService = new EvaluationFileService();
+            $evaluationFile = $evaluationFileService->update($data, $fileId);
             return (new EvaluationFileResource($evaluationFile))
                 ->response()
-                ->setStatusCode(200);
+                ->setStatusCode(201);
+        } catch (Throwable $e) {
+            Log::error('Message occured => ' . $e->getMessage());
+            return response()->json([], 400);
         }
     }
 
     public function preview($evaluationId, $fileId)
     {
+        // try {
+        //     $evaluationFile = EvaluationFile::find($fileId);
+        //     return response()->file(
+        //         storage_path('app/' . $evaluationFile->path)
+        //     );
+        // } catch (Throwable $e) {
+        //     Log::error('Message occured => ' . $e->getMessage());
+        //     return response()->json([], 400);
+        // }
+
         try {
-            $evaluationFile = EvaluationFile::find($fileId);
-            return response()->file(
-                storage_path('app/' . $evaluationFile->path)
-            );
+            $evaluationFileService = new EvaluationFileService();
+            return $evaluationFileService->preview($fileId);
         } catch (Throwable $e) {
             Log::error('Message occured => ' . $e->getMessage());
             return response()->json([], 400);
@@ -90,14 +129,24 @@ class EvaluationFileController extends Controller
      */
     public function destroy($evaluationId, $fileId)
     {
-        $file = EvaluationFile::find($fileId);
+        // $file = EvaluationFile::find($fileId);
 
-        Evaluation::find($evaluationId)
-            ->files()
-            ->where('id', $fileId)
-            ->first()
-            ->delete();
-        Storage::delete($file->path);
-        return response()->json([], 204);
+        // Evaluation::find($evaluationId)
+        //     ->files()
+        //     ->where('id', $fileId)
+        //     ->first()
+        //     ->delete();
+        // Storage::delete($file->path);
+        // return response()->json([], 204);
+
+        try {
+            $evaluationFileService = new EvaluationFileService();
+            if ($evaluationFileService->delete($fileId)) {
+                return response()->json([], 204);
+            }
+        } catch (Throwable $e) {
+            Log::error('Message occured => ' . $e->getMessage());
+            return response()->json([], 400);
+        }
     }
 }
