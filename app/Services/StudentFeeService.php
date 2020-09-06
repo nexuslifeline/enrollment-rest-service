e9e9af297de4dd2ab9cc9ad6d1f5856ff6742d6c
Jason Patawaran <patawaranjason17@gmail.com>
2020-09-06 21:04:59 +0800

    student fee service

--- app/Services/StudentFeeService.php
+++ app/Services/StudentFeeService.php
@@ -16,17 +16,15 @@ class StudentFeeService
         try {
             $studentFee = StudentFee::find($id);
             $studentFee->update($data);
-            if ($studentFeeItems ?? false) {
-                $fees = $studentFeeItems;
-                $items = [];
-                foreach ($fees as $fee) {
-                    $items[$fee['school_fee_id']] = [
-                        'amount' => $fee['amount'],
-                        'notes' => $fee['notes']
-                    ];
-                }
-                $studentFee->studentFeeItems()->sync($items);
+            $fees = $studentFeeItems;
+            $items = [];
+            foreach ($fees as $fee) {
+                $items[$fee['school_fee_id']] = [
+                    'amount' => $fee['amount'],
+                    'notes' => $fee['notes']
+                ];
             }
+            $studentFee->studentFeeItems()->sync($items);
         } catch (Exception $e) {
             Log::info('Error occured during update getStudentFeeOfTranscript method call: ');
             Log::info($e->getMessage());
@@ -42,36 +40,6 @@ class StudentFeeService
             return $studentFee;
         } catch (Exception $e) {
             Log::info('Error occured during StudentFeeService getStudentFeeOfTranscript method call: ');
-            Log::info($e->getMessage());
-            throw $e;
-        }
-    }
-
-    public function getStudentFeesOfStudent(int $studentId, bool $isPaginated, int $perPage, array $filters)
-    {
-        try {
-            $query = Student::findOrFail($studentId)->studentFees()
-            ->with([
-                'studentFeeItems',
-                'level',
-                'course',
-                'semester',
-                'schoolYear',
-                'student' => function ($query) {
-                    return $query->with(['address', 'photo']);
-            }]);
-            // application status
-            $studentFeeStatusId = $filters['student_fee_status_id'] ?? false;
-            $query->when($studentFeeStatusId, function($q) use ($studentFeeStatusId) {
-                return $q->where('student_fee_status_id', $studentFeeStatusId);
-            });
-
-            $studentFees = $isPaginated
-                ? $query->paginate($perPage)
-                : $query->get();
-            return $studentFees;
-        } catch (Exception $e) {
-            Log::info('Error occured during StudentFeeService getStudentFeesOfStudent method call: ');
             Log::info($e->getMessage());
             throw $e;
         }
